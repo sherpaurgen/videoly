40dea55e3588f08ef82acd15c7e904a8b4e46983
urgen sherpa <7010051+sherpaurgen@users.noreply.github.com>
2020-07-26 16:52:39 +0545

    state and handlers moved to main App.js, pass handlers as props to child component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
-import React from 'react';
-import Counters from './components/counters'
+import React, { Component } from 'react'
 //import Movies from './components/movies'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Counters from './components/counters';
+import NavBar from './components/navbar'
 
-function App() {
-  return (
-    <main className="container">
-      {/* <Movies /> */}
-      <Counters />
-    </main>
-  );
+export default class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleDelete = (counterId) => {
+    console.log("handle delete called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+
+  handleIncrement = (counter) => {
+    const countertemp = [...this.state.counters];
+    const index = countertemp.indexOf(counter);
+    countertemp[index] = { ...counter };
+    countertemp[index].value++;
+    this.setState({ counters: countertemp });
+  };
+
+  handleReset = () => {
+    const countertemp = this.state.counters.map((x) => {
+      x.value = 0;
+      return x;
+    });
+    this.setState({ counters: countertemp });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <main className="container">
+          <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+
+
+
+    )
+  }
 }
-
-export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,52 +2,22 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-  handleDelete = (counterId) => {
-    console.log("handle delete called", counterId);
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters: counters });
-  };
-
-  handleIncrement = (counter) => {
-    const countertemp = [...this.state.counters];
-    const index = countertemp.indexOf(counter);
-    countertemp[index] = { ...counter };
-    countertemp[index].value++;
-    this.setState({ counters: countertemp });
-  };
-
-  handleReset = () => {
-    const countertemp = this.state.counters.map((x) => {
-      x.value = 0;
-      return x;
-    });
-    this.setState({ counters: countertemp });
-  };
-
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-sm m-2 btn-success"
         >
           Reset
         </button>
 
-        {this.state.counters.map((c) => (
+        {this.props.counters.map((c) => (
           <Counter
             key={c.id}
             counter={c}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
           />
         ))}
       </div>
